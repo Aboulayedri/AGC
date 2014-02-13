2e264ec98ae048fa10f7f0587be6981b36cc9e6c
Abdoulaye SOUMARE <abdoulaye.soumare@devoteam.com>
2014-02-13 12:41:15 +0100

    retour sur l'authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_collaborator!
+  # before_action :authenticate_collaborator!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -2,24 +2,33 @@
 #
 # Table name: collaborators
 #
-#  id               :integer          not null, primary key
-#  prenom           :string(255)
-#  nom              :string(255)
-#  id_karma         :string(255)
-#  role             :string(255)
-#  email            :string(255)
-#  etat             :string(255)
-#  statut           :string(255)
-#  niv_diplome      :string(255)
-#  nat_diplome      :string(255)
-#  entity_id        :integer
-#  manager_id       :integer
-#  commentaire      :text
-#  created_at       :datetime
-#  updated_at       :datetime
-#  profil           :string(255)
-#  eligibilite      :string(255)
-#  aramis_entity_id :integer
+#  id                     :integer          not null, primary key
+#  prenom                 :string(255)
+#  nom                    :string(255)
+#  id_karma               :string(255)
+#  role                   :string(255)
+#  email                  :string(255)
+#  etat                   :string(255)
+#  statut                 :string(255)
+#  niv_diplome            :string(255)
+#  nat_diplome            :string(255)
+#  entity_id              :integer
+#  manager_id             :integer
+#  commentaire            :text
+#  created_at             :datetime
+#  updated_at             :datetime
+#  profil                 :string(255)
+#  eligibilite            :string(255)
+#  aramis_entity_id       :integer
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
 #
 
 class Collaborator < ActiveRecord::Base
